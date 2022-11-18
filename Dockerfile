65bbd9abfeb269edbba2e3afc39b893a6b47783c
emmanuel <emmanueldessaint27220@gmail.com>
2022-11-18 11:19:30 +0100

    first commit

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,6 @@ RUN cd /tmp && \
 
 WORKDIR /tmp/backend
 
-RUN touch .env && \
-    echo "PASSWORD=Rumutcho270?" >> .env
-
 RUN cd /tmp/backend && \
     npm install && \
     npm install -g nodemon
--- models/Materiel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const materielSchema = mongoose.Schema({
-  id: {type: String, required: true},
-  nom: { type: String, required: true },
-  stock: { type: Number, required: true },
-});
-
-module.exports = mongoose.model('Materiel', materielSchema);
